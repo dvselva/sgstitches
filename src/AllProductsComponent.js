08a1c90975acd63fa6b79fe2df3d55ae50539732
Selva <dvselva@gmail.com>
2022-05-09 04:50:36 -0400

    modal component addition

--- src/AllProductsComponent.js
+++ src/AllProductsComponent.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 // import Container from 'react-bootstrap/Container';
 // import Row from 'react-bootstrap/Row'
 // import Col from 'react-bootstrap/Col'
-// import Image from 'react-bootstrap/Image'
+import Image from 'react-bootstrap/Image'
 // import Carousel from 'react-bootstrap/Carousel'
-import LayoutComponent from  './layout/LayoutComponent';
+import LayoutComponent from './layout/LayoutComponent';
 
 // import * as contentful from 'contentful';
 import Spinner from 'react-bootstrap/Spinner';
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
 // import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 // import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
@@ -17,6 +19,9 @@ function AllProductsComponent(props) {
   const [items, setItems] = useState([]);
   const [newitems, setnewItems] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [show, setShow] = useState(false);
+  const [srcImageUrl, setsrcImageUrl] = useState("");
+  const [name, setName] = useState("");
 
 
 
@@ -25,6 +30,16 @@ function AllProductsComponent(props) {
     fetchNewProducts();
   }, []);
 
+  const handleClose = () => setShow(false);
+
+  const handleShow = (src,name) => {
+    if (src) {
+      setsrcImageUrl(src);
+      setName(name);
+    }
+    setShow(true);
+
+  }
 
   const splitItems = () => {
     var perChunk = Math.ceil(items.length / 3)
@@ -32,7 +47,7 @@ function AllProductsComponent(props) {
       const chunkIndex = Math.floor(index / perChunk)
 
       if (!resultArray[chunkIndex]) {
-        resultArray[chunkIndex] = [] 
+        resultArray[chunkIndex] = []
       }
 
       resultArray[chunkIndex].push(item)
@@ -40,83 +55,46 @@ function AllProductsComponent(props) {
       return resultArray
     }, [])
 
-    console.log(result); 
+    console.log(result);
     return result;
   }
 
 
 
-async function fetchProducts() {
-  try {
-    let response = await fetch('/api/products-get');
-    let data = await response.json();
-    setItems(data);
-    setLoading(false);
+  async function fetchProducts() {
+    try {
+      let response = await fetch('/api/products-get');
+      let data = await response.json();
+      setItems(data);
+      setLoading(false);
 
-  } catch(err) {
-    // catches errors both in fetch and response.json
-    alert(err);
+    } catch (err) {
+      // catches errors both in fetch and response.json
+      alert(err);
+    }
   }
-}
 
 
-async function fetchNewProducts() {
-  try {
-    let response = await fetch(process.env.REACT_APP_URL+ '/api/products-new-get');
-    let data = await response.json();
-    setnewItems(data);
-    setLoading(false);
-  } catch(err) {
-    // catches errors both in fetch and response.json
-    alert(err);
+  async function fetchNewProducts() {
+    try {
+      let response = await fetch('/api/products-new-get');
+      let data = await response.json();
+      setnewItems(data);
+      setLoading(false);
+    } catch (err) {
+      // catches errors both in fetch and response.json
+      alert(err);
+    }
   }
-}
-
-  // const fetchProducts = async () => {
-  //   let contentfulClient = contentful.createClient({
-  //     accessToken:  process.env.REACT_APP_CDKEY,
-  //     space: '9gf6mhyw2bkx'
-  //   });
-  //   let PLAYER_CONTENT_TYPE_ID = 'yesgeProducts';
-
-  //   contentfulClient.getEntries({
-  //     content_type: PLAYER_CONTENT_TYPE_ID
-  //   })
-  //     .then(function (entries) {
-  //       setItems(entries.items);
-  //       setLoading(false);
-
-  //     })
-  // }
-
-
-  // const fetchNewProducts = async () => {
-
-  //   let contentfulClient = contentful.createClient({
-  //     accessToken: '6o_DMSyLI7OSMmd434UXyAb2ILGS2R9F7c5h_lmsYWI',
-  //     space: '9gf6mhyw2bkx'
-  //   });
-  //   let PLAYER_CONTENT_TYPE_ID = 'yesgeProducts';
-  
-  //   contentfulClient.getEntries({
-  //     content_type: PLAYER_CONTENT_TYPE_ID,
-  //     'metadata.tags.sys.id[in]': 'new,latest'
-  //   })
-  //     .then(function (entries) {
-  //       setnewItems(entries.items);
-  //       setLoading(false);
-
-  //     })
-  // }
 
 
   const getContents = () => {
-   
+
     const resultsArray = splitItems(items);
 
-    const columns1Array =[];
-    const columns2Array =[];
-    const columns3Array =[];
+    const columns1Array = [];
+    const columns2Array = [];
+    const columns3Array = [];
 
     resultsArray.forEach((entries, outerindex) => {
 
@@ -126,57 +104,57 @@ async function fetchNewProducts() {
         let imageUrl = "https://placeholder.pics/svg/300";
 
         try {
-        if (entry.fields.image && entry.fields.image.length > 0) {
-          imageUrl = entry.fields.image[0].fields.file.url;
+          if (entry.fields.image && entry.fields.image.length > 0) {
+            imageUrl = entry.fields.image[0].fields.file.url;
+          }
         }
-      }
-      catch {
+        catch {
           console.log("error");
-      }
-      columnsArray.push(<div className="row mt-3">
-        <a href={imageUrl}>
-       <div className="col-md-12" >
-          <div className="card" >
-            <img src={imageUrl} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title" style={{textDecoration:"none",textUnderline:"none"}}>{entry.fields.name}</h5>
+        }
+        columnsArray.push(<div className="row mt-3">
+          <a  onClick={() => handleShow(imageUrl,entry.fields.name)}  >
+            <div className="col-md-12" >
+              <div className="card" >
+                <img src={imageUrl} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title" style={{ textDecoration: "none", textUnderline: "none" }}>{entry.fields.name}</h5>
+                </div>
+              </div>
             </div>
-          </div>
+          </a>
         </div>
-        </a>
-        </div>
-      )
+        )
       })
 
-      if (outerindex ===0 ){
+      if (outerindex === 0) {
         columns1Array.push(columnsArray);
       }
-      else if (outerindex ===1) {
-        columns2Array.push (columnsArray);
-        }
+      else if (outerindex === 1) {
+        columns2Array.push(columnsArray);
+      }
       else {
         columns3Array.push(columnsArray)
       }
 
     })
 
-    return <div><div className="header-style">{props.title}</div> <div className="row"> 
-        <div className="col-md-4">
-    {columns1Array}
-    </div>
-    <div className="col-md-4">
-    {columns2Array}
-    </div>
-    <div className="col-md-4">
-    {columns3Array}
-    </div>
+    return <div><div className="header-style">{props.title}</div> <div className="row">
+      <div className="col-md-4">
+        {columns1Array}
+      </div>
+      <div className="col-md-4">
+        {columns2Array}
+      </div>
+      <div className="col-md-4">
+        {columns3Array}
+      </div>
     </div>
     </div>;
   }
 
   const getTopContents = () => {
-      const contentsArray=[];
-     newitems.forEach((item, index) => {
+    const contentsArray = [];
+    newitems.forEach((item, index) => {
 
       console.log(item.fields.name);
       // let rawRichTextField = item.fields.description;
@@ -188,39 +166,65 @@ async function fetchNewProducts() {
         }
       }
       catch {
-          console.log("error");
+        console.log("error");
       }
 
-     contentsArray.push(
-      <a href={imageUrl}>
-        <div className="col-md-12 mt-3" >
-          <div class="card" >
-            <img src={imageUrl} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">{item.fields.name}</h5>
-             
+      contentsArray.push(
+        <a onClick={() => handleShow(imageUrl,item.fields.name)}>
+          <div className="col-md-12 mt-3" >
+            <div class="card" >
+              <img src={imageUrl} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">{item.fields.name}</h5>
+              </div>
             </div>
           </div>
-        </div>
         </a>
       )
-      
+
     });
 
     return <div> <div className="header-style">{props.title}</div>{contentsArray}</div>;
-  
+
   }
   return (
-    // <div className="container" style={{backgroundColor:"white",marginTop:"20px",borderRadius:"10px",paddingBottom:"20px"}}>
+    <>
       <LayoutComponent>
-      {!loading && props.mode==='all' ? getContents(): !loading && props.mode==='top' ? getTopContents():<div>
-      <Spinner animation="border" role="status" variant="danger">
-  <span className="visually-hidden">Loading...</span>
-</Spinner>
+        {!loading && props.mode === 'all' ? getContents()
 
-      </div> }
+
+          :
+          !loading && props.mode === 'top' ? getTopContents()
+            :
+            <div>
+              <Spinner animation="border" role="status" variant="danger">
+                <span className="visually-hidden">Loading...</span>
+              </Spinner>
+            </div>}
+
+
+            <Modal show={show}  dialogClassName="modal-80w" onHide={handleClose}  backdrop="static">
+<Modal.Header closeButton>
+  <Modal.Title>{name}</Modal.Title>
+</Modal.Header>
+<Modal.Body>
+  <Image src={srcImageUrl} fluid />
+</Modal.Body>
+<Modal.Footer>
+  <Button variant="secondary" onClick={handleClose}>
+    Close
+  </Button>
+  {/* <Button variant="primary" onClick={handleClose}>
+    Save Changes
+  </Button> */}
+</Modal.Footer>
+</Modal>
+
       </LayoutComponent>
-    // </div >
+
+
+
+    </>
   );
 }
 
