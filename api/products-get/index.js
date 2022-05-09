091dc6b1ab233f06134cf0189ace2a712b69da56
Selva <dvselva@gmail.com>
2022-05-08 20:38:34 -0400

    restructured

--- api/products-get/index.js
+++ api/products-get/index.js
@@ -1,26 +1,39 @@
 
 var contentful = require('contentful');
 
-module.exports = function (context, req) {
-    try {
-        let contentfulClient = contentful.createClient({
-            accessToken: process.env["CDAPI_KEY"],
-            space: '9gf6mhyw2bkx'
-          });
-          let PLAYER_CONTENT_TYPE_ID = 'yesgeProducts';
-        
-          contentfulClient.getEntries({
-            content_type: PLAYER_CONTENT_TYPE_ID
-          })
-            .then(function (entries) {
-            context.res.status(200).json(entries.items);
-            context.done();
-           })
+let contentfulClient = contentful.createClient({
+  accessToken: process.env["CDAPI_KEY"],
+  space: '9gf6mhyw2bkx'
+});
+let PLAYER_CONTENT_TYPE_ID = 'yesgeProducts';
+const call = contentfulClient.getEntries({content_type: PLAYER_CONTENT_TYPE_ID})
+
+module.exports = async function (context, req) {
+  let data;  
+  try {
+      
+        // let contentfulClient = contentful.createClient({
+        //     accessToken: process.env["CDAPI_KEY"],
+        //     space: '9gf6mhyw2bkx'
+        //   });
+        //   let PLAYER_CONTENT_TYPE_ID = 'yesgeProducts';
+          
+          data = await call;
+
+          // contentfulClient.getEntries({
+          //   content_type: PLAYER_CONTENT_TYPE_ID
+          // })
+          //   .then(function (entries) {
+          //   context.res.status(200).json(entries.items);
+          //   context.done();
+          //  })
 
         }
     catch (error) {
-        context.res.status(500).send(error);
-        context.done();
+      context.log.error('ERROR', err);
+      throw err;
+        // context.res.status(500).send(error);
+       
     }
-    context.done();
+    context.res.status(200).json(data.items);
 }
