16764670483361ff18ddc1d06bbdb40741bf53de
Selva <dvselva@gmail.com>
2022-05-08 21:32:01 -0400

    fine tunning

--- api/products-get/index.js
+++ api/products-get/index.js
@@ -6,34 +6,18 @@ let contentfulClient = contentful.createClient({
   space: '9gf6mhyw2bkx'
 });
 let PLAYER_CONTENT_TYPE_ID = 'yesgeProducts';
-const call = contentfulClient.getEntries({content_type: PLAYER_CONTENT_TYPE_ID})
+const cdi = contentfulClient.getEntries({ content_type: PLAYER_CONTENT_TYPE_ID })
 
 module.exports = async function (context, req) {
-  let data;  
+  let data;
   try {
-      
-        // let contentfulClient = contentful.createClient({
-        //     accessToken: process.env["CDAPI_KEY"],
-        //     space: '9gf6mhyw2bkx'
-        //   });
-        //   let PLAYER_CONTENT_TYPE_ID = 'yesgeProducts';
-          
-          data = await call;
+    data = await cdi;
+  }
+  catch (error) {
+    context.log.error('ERROR', err);
+    throw err;
+    // context.res.status(500).send(error);
 
-          // contentfulClient.getEntries({
-          //   content_type: PLAYER_CONTENT_TYPE_ID
-          // })
-          //   .then(function (entries) {
-          //   context.res.status(200).json(entries.items);
-          //   context.done();
-          //  })
-
-        }
-    catch (error) {
-      context.log.error('ERROR', err);
-      throw err;
-        // context.res.status(500).send(error);
-       
-    }
-    context.res.status(200).json(data.items);
+  }
+  context.res.status(200).json(data.items);
 }
