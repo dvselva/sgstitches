5c8c304ad5d9e0fe806f7f8d354472da2cb4befb
Selva <dvselva@gmail.com>
2022-05-09 05:28:02 -0400

    layout padding

--- src/layout/LayoutComponent.js
+++ src/layout/LayoutComponent.js
@@ -6,7 +6,7 @@ import Container from 'react-bootstrap/Container';
 const LayoutComponent =({children}) =>{
     return(
         <>
-        <Container style={{ backgroundColor: "white", marginTop: "20px", borderRadius: "10px", padding: "50px" }}>
+        <Container style={{ backgroundColor: "white", marginTop: "20px", borderRadius: "10px", paddingBottom: "25px" }}>
         <main>{children}</main>
         </Container>
         </>
